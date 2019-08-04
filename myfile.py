926a2332eaf677d61cf22af7e637c9244c48b98d
manasi1994 <manasi.khanuja@gmail.com>
2019-08-04 15:20:52 -0700

    Day2 changes

--- myfile.py
+++ myfile.py
@@ -1,3 +1,4 @@
+import sys
 print("This is myfile!")
 x=15
 y=1
@@ -8,37 +9,12 @@ c="again"
 print("x/y = ", x/y)
 myList=[1,2,3,4,5]
 i=0
-#for i in myList:
- #   print(i)
 
-def myPrint(words):
-    print(words,"ok")
+def main():
+    print(sys.argv, type(sys.argv))
+    print(len(sys.argv))
+    print(sys.argv[0])
+    #print(sys.argv[1])
 
-#myPrint("Are you")
-
-def myFunc(vList):
-    for i in vList:
-        print(i)
-#myFunc([1,2,"WHYY",4,"hello"])   
-
-def myRecursFunc(vList):
-    if len(vList)>0:
-        #print(vList.pop())
-        myRecursFunc(vList)
-#myRecursFunc([1,2,3,4,5])
-
-while(x>0):
-    #print(x)
-    x-=1
-def example2(start,vList,step):
-    for i in range(start,len(vList),step):
-        print(vList[i])
-
-def example21(start,vList,step):
-    if(i<len(vList)):
-        print(vList.pop)
-        example21(start,vList,step)
-
-
-
-#example21(0,[3,4,5,6,7,9,10,11],2)
+if __name__=='__main__':
+    main()
